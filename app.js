1571d4a60081278f63e37995cad62cc5cb2e4bc7
Michael Clark <hudsonriver4151@gmail.com>
2025-07-29 22:05:34 -0800

    Initial commit for Hoffman's Reptile Shop

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Set EJS as template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for /home
+app.get('/home', (req, res) => {
+    res.render('index', { title: 'Home Page' });
+});
+
+// Default route (redirect / to /home)
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
