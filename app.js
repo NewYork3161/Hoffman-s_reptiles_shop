7bafab591380179de32d226207bfe0478122ca7f
Michael Clark <hudsonriver4151@gmail.com>
2025-07-29 23:08:09 -0800

    Prepare app for Render deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Dynamic port for Render
 
 // Set EJS as template engine
 app.set('view engine', 'ejs');
