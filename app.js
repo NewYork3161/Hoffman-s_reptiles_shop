d71709d3a3daa9fac0594830f3427d2e44f144ba
Michael Clark <hudsonriver4151@gmail.com>
2025-07-30 13:47:16 -0800

    Add sitemap and robots routes

--- app.js
+++ app.js
@@ -4,21 +4,31 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000; // Dynamic port for Render
 
-// Set EJS as template engine
+// Set EJS as the template engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from 'public' folder
+// Serve static assets from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for /home
+// Route for Home Page
 app.get('/home', (req, res) => {
     res.render('index', { title: 'Home Page' });
 });
 
-// Default route (redirect / to /home)
+// Default route redirects to /home
 app.get('/', (req, res) => {
     res.redirect('/home');
+});
+
+// Serve sitemap.xml explicitly
+app.get('/sitemap.xml', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sitemap.xml'));
+});
+
+// Serve robots.txt explicitly
+app.get('/robots.txt', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'robots.txt'));
 });
 
 // Start the server
