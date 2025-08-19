0a1df69faaaa1031a24c14713867f09f9d00a5a1
Michael Clark <hudsonriver4151@gmail.com>
2025-08-19 02:22:48 -0700

    Update project files

--- app.js
+++ app.js
@@ -16,6 +16,55 @@ app.get('/home', (req, res) => {
     res.render('index', { title: 'Home Page' });
 });
 
+// Animals Page
+app.get('/animals', (req, res) => {
+    res.render('animals', { title: 'Animals' });
+});
+
+// Individual Animal Pages
+app.get('/lizards', (req, res) => {
+    res.render('lizards', { title: 'Lizards' });
+});
+app.get('/snakes', (req, res) => {
+    res.render('snakes', { title: 'Snakes' });
+});
+app.get('/turtles', (req, res) => {
+    res.render('turtles', { title: 'Turtles' });
+});
+app.get('/tortoises', (req, res) => {
+    res.render('tortoises', { title: 'Tortoises' });
+});
+app.get('/frogs', (req, res) => {
+    res.render('frogs', { title: 'Frogs' });
+});
+app.get('/rats', (req, res) => {
+    res.render('rats', { title: 'Rats' });
+});
+app.get('/tarantulas', (req, res) => {
+    res.render('tarantulas', { title: 'Tarantulas' });
+});
+app.get('/newts', (req, res) => {
+    res.render('newts', { title: 'Newts' });
+});
+app.get('/salamanders', (req, res) => {
+    res.render('salamanders', { title: 'Salamanders' });
+});
+
+// Gallery Page
+app.get('/gallery', (req, res) => {
+    res.render('gallery', { title: 'Gallery' });
+});
+
+// About Page
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Us' });
+});
+
+// Contact Page
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact' });
+});
+
 // Default route redirects to /home
 app.get('/', (req, res) => {
     res.redirect('/home');
